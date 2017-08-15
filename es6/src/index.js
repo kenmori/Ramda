95a4e6cf61c6c127c6340dc4edb8ba5d83b47e3f
森田賢二 <kenjimoritata@gmail.com>
2017-08-15 20:29:13 +0900

    Add

--- es6/src/index.js
+++ es6/src/index.js
@@ -5,3 +5,22 @@ console.log(f(10));
 
 var ff = (obj) => console.log(obj)
 var resultff = R.tap(ff, {x: "kenji"});
+
+
+var pathed = R.path(['a', 'e'], {a: {b: 2, c: 9}});
+console.log(pathed);
+
+var obj = {
+    body: {
+        data: {
+            name: 'morita'
+        }
+    }
+};
+const func = R.pathOr('', ['body', 'data', 'name']);
+console.log(func(obj));
+
+
+var duplicate = n => [n, n];
+R.chain(duplicate, [1,2,3]);
+
