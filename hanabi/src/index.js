e5d1196ff234b041d0c8cadab57e9cfbc4b474d1
森田賢二 <kenjimoritata@gmail.com>
2017-08-19 08:30:35 +0900

    Add Stream

--- hanabi/src/index.js
+++ hanabi/src/index.js
@@ -6,6 +6,42 @@ import 'lodash';
 
 class Greet extends Component {
  render() {
+
+  var stream = {
+   match: (data, pattern) => {
+       return data(pattern);
+      },
+  empty: (_) => {
+   return (pattern) => {
+    return pattern.empty();
+   }
+  },
+  cons: (head, tailThunk) => {
+   return (pattern) => {
+    return pattern.cons(head, tailThunk);
+   }
+  },
+  head: (aStream) => {
+   return stream.match(aStream, {
+    empty: (_) => {return null;},
+    cons: (value, tailThunk) => {return value;}
+   });
+  },
+  tail: (aStream) => {
+   return stream.match(aStream, {
+    empty: (_) => {return null;},
+   cons: (head, tailThunk) => {
+    return tailThunk();
+   }
+   })
+  }
+
+  }
+  var theStream = staream.cons(1, (_)=>{
+       return stream.cons(2, (_)=>{
+        return stream.empty();
+       })
+      })
   return (
    <ul id='result'></ul>
   )
