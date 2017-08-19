67eb2de2bed1a4aa8c19b7e05d9f2d3947b0834b
森田賢二 <kenjimoritata@gmail.com>
2017-08-19 16:08:08 +0900

    wip

--- hanabi/src/index.js
+++ hanabi/src/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import 'babel-polyfill';
+import anime from 'animejs';
 import 'lodash';
+import R from 'ramda';
 
 
 //ボタンの上部100pxぐらいから丸い玉が上に登って行くアニメーション
@@ -22,6 +24,9 @@ import 'lodash';
 //document.getElementById('divName').style.setProperty("top", "100px");
 //window.getComputedStyle(element, null);
 //プラスの数値をマイナスにして文字列で返す
+//documentFragment
+//rangeをネイティブでどう作るか
+
 
 
 class Greet extends Component {
@@ -30,6 +35,8 @@ class Greet extends Component {
         this.uchiage = this.uchiage.bind(this);
         this.createHanabi = this.createHanabi.bind(this);
         this.animationUp = this.animationUp.bind(this);
+        this.createSpan = this.createSpan.bind(this);
+        this.hanabiCSSSet = this.hanabiCSSSet.bind(this);
         this.state = {
             id : 0
         }
@@ -42,37 +49,59 @@ class Greet extends Component {
         var span = document.createElement('span');
         span.style.left = x + 'px';
         span.style.top = y + 'px';
-        span.style.position = 'absolute';
-        span.style.display = 'inline-block';
         span.style.background = '#000';
-        span.style.width = '10px';
-        span.style.height = '10px';
+        span.classList.add('spanUp');
         span.id = this.state.id + 1;
         this.setState({id: this.state.id + 1});
         console.log(span);
         document.body.appendChild(span);
         this.animationUp(span.id);
+        this.createSpan(R.range(1, 9))//spanを作る
+        this.hanabiCSSSet();
+
+    }
+    hanabiCSSSet(){
+
+    }
+    createSpan (value){
+        var df = document.createDocumentFragment();
+        for (var i = 0; i <= value.length; i++){
+            var span = document.createElement('span');
+            span.classList.add('default');
+            span.classList.add('span' + i);
+            if(i === 0) span.classList.add('deg0');
+            else if (i === 1) span.classList.add('deg45');
+            else if (i === 2) span.classList.add('deg90');
+            else if (i === 3) span.classList.add('deg135');
+            else if (i === 4) span.classList.add('deg180');
+            else if (i === 5) span.classList.add('deg225');
+            else if (i === 6) span.classList.add('deg270');
+            else if (i === 7) span.classList.add('deg315');
+            else if(i === 8) span.classList.add('deg360');
+            df.appendChild(span);
+        }
+        var iddom = document.getElementById(this.state.id);
+
+        iddom.appendChild(df);
     }
 
     animationUp(id){
-        var thisspan = document.getElementById(id);
-            var intedTop = parseInt(thisspan.style.top.split("px")[0]);
-            thisspan.style.top =  (intedTop + 50) + 'px';
-        var move = () => {
-            var pos = 0;
-            var id = setInterval(frame, 5);
-            function frame(){
-                if(pos == 200){
-                    clearInterval(id);
-                } else {
-                    pos++;
-                    thisspan.style.top = parseInt(thisspan.style.top.split("px")[0]) + pos + 'px';
-                }
-            }
+        var span = document.getElementById(id);
+        var val = Math.floor(Math.random() * 10);
+        if(val === 0 || 1 ){
+            val = 3;
         }
-        move();
 
-
+        var value = val * -100;
+        anime({
+            targets: span,
+            translateY: [
+                {value: value, duration: 1200}
+            ],
+            rotate: '1turn',
+            duration: 10,
+            easing: 'linear'
+        })
     }
     uchiage(e){
         var targetX = e.pageX;
@@ -80,6 +109,7 @@ class Greet extends Component {
         this.createHanabi(targetX, targetY);
     }
  render(){
+     console.log(anime);
   return (
    <ul id='result'>
              <div id='js-view' className='view'></div>
